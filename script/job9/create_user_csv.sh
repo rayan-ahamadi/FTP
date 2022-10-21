161862b0f980d0ef7828e3597960c11680e6b95a
rayan <rayan@debian>
2022-10-21 12:46:43 +0200

    modification job 9

--- script/job9/create_user_csv.sh
+++ script/job9/create_user_csv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash 
 
-fichier=0 
+$1
 
-while IFS=, read -r col1 col2 col3 col4 col5 do
+while IFS=, read -r col1 col2 col3 col4 col5 
+do
  username="$col2_$col3"
  mdp="$col4"
  role="$col5"
@@ -10,8 +11,7 @@ while IFS=, read -r col1 col2 col3 col4 col5 do
 
  if [ $username != "Nom" ]; then
    sudo useradd -m $username && sudo usermod -p $mdp $username;
-   echo $mdp | ftpasswd --stdin --passwd --file=/etc/proftpd/ftp.passwd --name=$username --uid=60
-   --gid=60 --home=/home/$username/ --shell=/bin/false
+   echo $mdp | sudo ftpasswd --stdin --passwd --file=/etc/proftpd/ftp.passwd --name=$username --uid=60 gid=60 --home=/home/$username --shell=/bin/false
  fi
 
  if [ $role=="Admin" ]; then
@@ -19,4 +19,4 @@ while IFS=, read -r col1 col2 col3 col4 col5 do
  fi
 
 
-done < $fichier
+done < $1
